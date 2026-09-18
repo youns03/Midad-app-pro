8c4416c4ebace4feeec2db7cfabb330d3e5d7d46
youns03 <negane470@gmail.com>
2026-09-18 09:50:14 +0100

    Use Android default debug signing for CI builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -46,7 +40,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug {}
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
